8e74e68b06dd9357beb6bd3ce477d8a5dd00e10c
brianaj <brianaj22@gmail.com>
2019-08-01 12:53:50 -0700

    CitiesController Change

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,12 +1,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace CityInfo.API.Controllers
 {
     [Route("api/cities")]
     public class CitiesController : Controller
     {
+        private readonly ILogger logger;
+        public CitiesController(ILogger<CitiesController> logger)
+        {
+            this.logger = logger;
+        }
         [HttpGet()]
         public IActionResult GetCities()
         {
@@ -16,6 +22,7 @@ namespace CityInfo.API.Controllers
         [HttpGet("{id}")]
         public IActionResult GetCity(int id)
         {
+            logger.LogInformation("The city get method is being called");
             var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
             if (cityToReturn == null)
             {
